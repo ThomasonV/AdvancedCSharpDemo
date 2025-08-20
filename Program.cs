bada7eee27af63b608c8a8b1b052f538e6c61580
Thomason <vokurka.thomas@gmail.com>
2025-08-20 20:27:06 +0200

    Add new functions to Calculator

--- Program.cs
+++ Program.cs
@@ -2,6 +2,10 @@
 Calculator calc = new Calculator();
 int sum = calc.Add(5, 7);
 int product = calc.Multiply(3, 4);
+int deleno = calc.Divide(8, 3);
+int minus = calc.Subtract(12, 4);
 
 Console.WriteLine($"Sum: {sum}");
 Console.WriteLine($"Product: {product}");
+Console.WriteLine($"Deleno: {deleno}");
+Console.WriteLine($"Minus: {minus}");
