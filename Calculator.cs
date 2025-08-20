9a7068186291ab2b7ef986bc5dd825fb6ca1b2a6
Thomason <Vokurka.Thomas@gmail.com>
2025-08-20 13:37:04 +0200

    Přidání kalkulačky.

---
+++ Calculator.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class Calculator
+{
+	public int Add(int x, int y) { return x + y; }
+	public int Subtract(int x, int y) { return x - y; }
+	public int Multiply(int x, int y) {return x * y; }
+	public int Divide(int x, int y) { return x / y; }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Calculator calc = new Calculator();
+int sum = calc.Add(5, 7);
+int product = calc.Multiply(3, 4);
+
+Console.WriteLine($"Sum: {sum}");
+Console.WriteLine($"Product: {product}");
